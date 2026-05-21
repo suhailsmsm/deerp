f2c28519cdaa4d59f66e8d2248be85912d2f4eab
delemonqatar <delemonqatar@gmail.com>
2026-05-21 05:25:20 +0400

    Add pos_erp_system.css to main entry point

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import POSPage from './src/pages/page'
 import './src/index.css'
+import './src/pos_erp_system.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
